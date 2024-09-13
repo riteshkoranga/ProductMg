4af7c285d91149937514ae98fc5e07cb06080648
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-13 14:45:31 +0530

    cross origin to frontend added in copntroller class

--- src/main/java/com/Product/management/controller/productController.java
+++ src/main/java/com/Product/management/controller/productController.java
@@ -13,11 +13,13 @@ import com.Product.management.model.Product;
 import com.Product.management.service.service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/product")
 public class productController {
