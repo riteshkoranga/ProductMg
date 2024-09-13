24f167c20087107e0c789bc80bcff1604b8afe56
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-13 16:13:43 +0530

    web controler

--- src/main/java/com/Product/management/controller/productController.java
+++ src/main/java/com/Product/management/controller/productController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@CrossOrigin(origins = "https://product-mg-b6514418ec5c.herokuapp.com/")
+@CrossOrigin(origins = "https://product-mg-b6514418ec5c.herokuapp.com")
 @RestController
 @RequestMapping("/product")
 public class productController {
